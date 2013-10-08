bc9dc1b75f5e30e8ccfc1c52e64604af0c4fcf10
antonio <antonio@antonio-laptop>
2013-10-08 12:41:50 +0200

    asdf

---
+++ src/kVecinos/KVecinos.java
@@ -0,0 +1,118 @@
+package kVecinos;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class KVecinos {
+	
+	public static HashMap<Character, ArrayList<String>> filtraCosas(){
+		// 26 clases
+		HashMap<Character, ArrayList<String>> mapaco = new HashMap<Character, ArrayList<String>>();
+		Character inicial = 'A';
+		// Inicializo el hashmap
+		for(int i=0; i<26; i++){
+			ArrayList<String> letra = new ArrayList<>();
+			mapaco.put(inicial, letra);
+			System.out.println(inicial);
+			inicial++;
+		}
+		try {
+			// Leo y meto (no es un chiste sobre leo)
+			BufferedReader br1 = new BufferedReader(new FileReader(new File("Training.200.cad")));
+			String line = br1.readLine();
+			while(line != null){
+				try {
+					System.out.println(line.charAt(0));
+					mapaco.get(line.charAt(0)).add(line);
+				} catch (Exception ex){
+				}
+				line = br1.readLine();
+			}
+			
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		// Escritura de los archivos
+		System.out.println("Vamos a hacer archivos!!!!!");
+		Character ini = 'A';
+		try{
+			for(int f=0; f<5; f++){
+				BufferedWriter bw = new BufferedWriter(new FileWriter(new File("salida"+f+".txt")));
+				ini = 'A';
+				for(int i=0; i<26; i++){
+					for(int j=0; j<40; j++){
+						System.out.println(mapaco.get(ini).get(0));
+						bw.write(mapaco.get(ini).get(0)+"\n");
+						mapaco.get(ini).remove(0);
+					}
+					ini++;
+				}
+				bw.close();
+			}
+		} catch (Exception ex){
+			System.out.println(ex.getMessage());
+		}
+		
+		
+		return mapaco;
+		
+	}
+	
+	public static void CalculoDistanciaMinima(){
+		try {
+			BufferedReader br1 = new BufferedReader(new FileReader(new File("training.cad")));
+			BufferedReader br2 = new BufferedReader(new FileReader(new File("test.cad")));
+//			BufferedWriter bw = new BufferedWriter(new FileWriter(new File("salida.txt")));
+			ArrayList<String> lTraining = new ArrayList<>();
+			ArrayList<String> lTest = new ArrayList<>();
+			String line = br1.readLine();
+			while(line != null){
+				lTraining.add(line);
+				line = br1.readLine();
+			}
+			line = br2.readLine();
+			while(line != null){
+				lTest.add(line);
+				line = br2.readLine();
+			}
+			int aciertos=0;
+			System.out.println(lTraining.size());
+			System.out.println(lTest.size());
+			int distanciaMin = Integer.MAX_VALUE;
+			String etiquetaMin = "";
+			for(String item : lTest){
+				distanciaMin = Integer.MAX_VALUE;
+				for(String item2 : lTraining){
+					int distancia = Levenshtein.computeLevenshteinDistance(item.split(" ")[1], item2.split(" ")[1]);
+					if(distancia < distanciaMin){
+						distanciaMin = distancia;
+						etiquetaMin = item2.split(" ")[0];
+					}					
+				}
+				System.out.println("Este tiene la etiqueta " + item.split(" ")[0] + " y la mínima es " + etiquetaMin);
+				if(item.split(" ")[0].equals(etiquetaMin))
+					aciertos++;
+			}
+			System.out.println("El índice de acierto es de "+ (double)aciertos/lTest.size()*100.0);
+			
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	public static void main(String[] args) {
+		
+		HashMap<Character, ArrayList<String>> mapaco = filtraCosas();
+	}
+
+}
